07f4e7d131d5f7dab92cfae727f4f9c4e5623eb8
OperationRecon <mohammadnaas203@gmail.com>
2024-12-05 10:48:10 +0200

    added linear MLPs for the XOR problem

---
+++ Nueron_Layer.py
@@ -0,0 +1,68 @@
+from EDF_Percpetron import *
+import numpy as np
+
+class Nueron_Layer:
+    # The Nueron Layer base calss that input, and computational nodes are built upon
+
+    def __init__(self, input_layer = None, width: int = 1,):
+        self.input_layer = input_layer
+        self.width = width
+        self.output_layer = None
+        self.value = None
+        self.nodes = None
+        if self.input_layer:
+            self.input_layer.output_layer = self
+    
+    def forward(self):
+        # perform forward pass
+        raise NotImplementedError
+    
+    def backward(self):
+        # perform backward pass
+        raise NotImplementedError
+    
+class Input_Layer(Nueron_Layer):
+    # The layer used for processing inputs
+    def __init__(self, number_of_inputs):
+        Nueron_Layer.__init__(self, input_layer= None, width= number_of_inputs)
+        self.nodes = [Input()]
+        
+    def forward(self, value = None):
+        self.value = value if value else self.value
+        self.nodes[0].forward(self.value)
+    
+    def backward(self):
+        self.nodes[0].backward()
+    
+class Computation_Layer(Nueron_Layer):
+    # computation layers contain a nueron with an operation and activation equations, as well as trainable paramters
+    # paramter factory is a function used to generate the parameter vectors according to specified width
+    def __init__(self, input_layer=None, width = 1, operation: Node = Linear, activation: Node = Sigmoid, parameter_factory = None):
+        Nueron_Layer.__init__(self, input_layer, width)
+        self.paramter_nodes = parameter_factory(self.width, self.input_layer.width)
+        self.operation_node = operation(self.paramter_nodes, self.input_layer.nodes[-1])
+        self.activation_node = activation(self.operation_node)
+        self.nodes = [*self.paramter_nodes, self.operation_node, self.activation_node]
+
+    def forward(self):
+        for n in self.nodes:
+            n.forward()
+        
+    def backward(self):
+        # backward and update the paramters
+        for n in self.nodes[::-1]:
+            n.backward()
+        
+    
+    def grad_update(self, learning_rate = 0.001):
+        for n in self.paramter_nodes:
+            
+            try:
+                avg = np.average(n.gradients[n],axis=len(n.gradients[n].shape)-1, keepdims=True)
+                tmp = learning_rate * avg
+                n.value -= tmp
+
+            except:
+                avg = np.average(n.gradients[n],axis=len(n.gradients[n].shape)-1, keepdims=False)
+                tmp = learning_rate * avg
+                n.value -= tmp
---
+++ XOR_MLP.py
@@ -0,0 +1,183 @@
+from EDF_Percpetron import *
+from Nueron_Layer import *
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
+import time
+
+# Define constants
+CLASS1_SIZE = 200
+CLASS2_SIZE = 200
+N_FEATURES = 2
+N_OUTPUT = 1
+LEARNING_RATE = 0.8
+EPOCHS = 10000
+TEST_SIZE = 0.25
+BATCH_SIZE = int((CLASS1_SIZE + CLASS2_SIZE) * TEST_SIZE)
+DEPTH = 4
+WIDTH = 20
+
+# Define the means and covariances of the two components
+MEAN1a = np.array([-2, -4])
+COV1a = np.array([[1, 0], [0, 1]])
+MEAN1b = np.array([3, 2])
+COV1b = np.array([[1, 0], [0, 1]])
+MEAN2a = np.array([0, 0])
+COV2a = np.array([[1, 0], [0, 1]])
+MEAN2b = np.array([2, -4])
+COV2b = np.array([[1, 0], [0, 1]])
+
+# Generate random points from the two components
+X1 = np.vstack((multivariate_normal.rvs(MEAN1a, COV1a, int(np.floor(CLASS1_SIZE/2))),multivariate_normal.rvs(MEAN1b, COV1b, int(np.ceil(CLASS1_SIZE/2)))))
+X2 = np.vstack((multivariate_normal.rvs(MEAN2a, COV2a, int(np.floor(CLASS2_SIZE/2))), multivariate_normal.rvs(MEAN2b, COV2b, int(np.ceil(CLASS2_SIZE/2)))))
+
+# Combine the points and generate labels
+X = np.vstack((X1, X2))
+y = np.hstack((np.zeros(CLASS1_SIZE), np.ones(CLASS2_SIZE)))
+
+# Plot the generated data
+plt.scatter(X[:, 0], X[:, 1], c=y)
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.title('Scatter Plot of Generated Data')
+plt.show()
+
+# Split data
+indices = np.arange(X.shape[0])
+np.random.shuffle(indices)
+
+test_set_size = int(len(X) * TEST_SIZE)
+test_indices = indices[:test_set_size]
+train_indices = indices[test_set_size:]
+
+X_train, X_test = X[train_indices], X[test_indices]
+y_train, y_test = y[train_indices], y[test_indices]
+
+
+
+# Model parameters
+
+# initialize arrays to store evaluation results
+times = []
+fn_losses = []
+accuracies = []
+
+
+# Create nodes
+y_node = Input()
+
+def linear_param_builder(w,input_layer_w):
+    # the parameter factory used for the linear function
+    return [Parameter(np.zeros((w,1))), 
+            Parameter(np.random.default_rng().standard_normal(size=(input_layer_w,w))*0.1)]
+
+# Build computation graph
+graph = []
+# build first layer
+input_layer = Input_Layer(N_FEATURES)
+graph.append(input_layer)
+
+# build hidden layers
+# hidden_layer1 = Computation_Layer(input_layer, 20, parameter_factory=linear_param_builder, operation=Linear, activation=Sigmoid)
+# hidden_layer2 = Computation_Layer(hidden_layer1, 20, parameter_factory=linear_param_builder, operation=Linear, activation=Sigmoid)
+for i in range(1,DEPTH+1):
+    graph.append(Computation_Layer(graph[i-1],
+                                    WIDTH,
+                                    parameter_factory=linear_param_builder, 
+                                    operation=Linear, activation=Sigmoid))
+
+# build output layer
+output_layer = Computation_Layer(graph[-1], N_OUTPUT, Linear, Sigmoid, linear_param_builder)
+
+loss = BCE(y_node, output_layer.nodes[-1])
+
+graph.extend([output_layer, loss])
+
+# Create graph outside the training loop
+trainable = graph[1:-1]
+
+# Training loop
+epochs = EPOCHS
+learning_rate = LEARNING_RATE
+
+# Forward and Backward Pass
+def forward_pass(graph):
+    for n in graph:
+        n.forward()
+
+def backward_pass(graph):
+    for n in graph[::-1]:
+        n.backward()
+
+# SGD Update
+def sgd_update(trainables, learning_rate=1e-2):
+    for t in trainables:
+        t.grad_update(learning_rate)
+
+
+# Start a measure of the model's excecutrion speed
+start = time.time()
+
+losses = []
+
+for epoch in range(epochs):
+    loss_value = 0
+    for i in range(0,X_train.shape[0], BATCH_SIZE):
+        x1 = X_train[i:min(X_train.shape[0], i + BATCH_SIZE), 0].reshape(1,-1)
+        x2  = X_train[i:min(X_train.shape[0], i + BATCH_SIZE), 1].reshape(1, -1)
+        input_layer.value = np.vstack((x1,x2,))
+        y_node.value = y_train[i:min(X_train.shape[0], i + BATCH_SIZE)].reshape(1, -1)
+
+        forward_pass(graph)
+        backward_pass(graph)
+        sgd_update(trainable, learning_rate)
+
+        loss_value += loss.value
+
+    losses.append(loss_value / X_train.shape[0])
+    print(f'Epoch: {epoch + 1}, loss: {loss_value / X_train.shape[0]}')
+
+end = time.time()
+times.append(end-start)
+fn_losses.append(loss_value / X_train.shape[0])
+
+print(f"Size {BATCH_SIZE}, Loss: {loss_value / X_train.shape[0]}\n Time: {(end - start):.4f} seconds")
+
+# Evaluate the model
+correct_predictions = 0
+for i in range(X_test.shape[0]):
+    input_layer.value = np.vstack((X_test[i][0].reshape(-1, 1),X_test[i][1].reshape(-1, 1)))
+    forward_pass(graph)
+
+    if round(output_layer.activation_node.value[0][0]) == y_test[i]:
+        correct_predictions += 1
+
+accuracy = correct_predictions / X_test.shape[0]
+accuracies.append(accuracy)
+
+print(f"Accuracy: {accuracy * 100:.3f}%")
+
+
+x_min, x_max = X[:, 0].min(), X[:, 0].max()
+y_min, y_max = X[:, 1].min(), X[:, 1].max()
+xx, yy = np.meshgrid(np.linspace(x_min, x_max), np.linspace(y_min, y_max))
+Z = []
+for i,j in zip(xx.ravel(),yy.ravel()):
+    input_layer.value = np.vstack((np.array([i]), np.array([j])))
+    forward_pass(graph)
+    Z.append(output_layer.activation_node.value)
+Z = np.array(Z).reshape(xx.shape)
+
+plt.contourf(xx, yy, Z, alpha=0.8)
+plt.scatter(X[:, 0], X[:, 1], c=y)
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.title('Decision Boundary')
+plt.show()
+
+plt.plot(range(1,EPOCHS+1), losses,)
+plt.xlabel('epoch')
+plt.ylabel('Loss')
+plt.title('Loss Vs. Batch Size')
+plt.show()
+
