460f5c5c5eda45038535eb0f1a9c1016dad4b15c
OperationRecon <mohammadnaas203@gmail.com>
2024-12-14 23:19:59 +0200

    added mlp learning for full_mnist dataset

--- assignment1/full_MNISt.py
+++ assignment1/full_MNISt.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot
 
 #loading the dataset
 (train_X, train_y), (test_X, test_y) = keras.datasets.mnist.load_data()
-(train_X, train_y), (test_X, test_y) = (train_X[:2000], train_y[:2000]), (test_X[:100], test_y[:100])
+(train_X, train_y), (test_X, test_y) = (train_X, train_y), (test_X, test_y)
 #printing the shapes of the vectors
 print('X_train: ' + str(train_X.shape))
 print('Y_train: ' + str(train_y.shape))
@@ -16,27 +16,30 @@ print('X_test:  '  + str(test_X.shape))
 print('Y_test:  '  + str(test_y.shape))
 
 
-for i in range(9):
-    pyplot.subplot(330 + 1 + i)
-    pyplot.imshow(train_X[i], cmap=pyplot.get_cmap('gray'))
-pyplot.show()
+# for i in range(9):
+#     pyplot.subplot(330 + 1 + i)
+#     pyplot.imshow(train_X[i], cmap=pyplot.get_cmap('gray'))
+# pyplot.show()
 
 train_X = train_X.reshape(-1,28*28)
 test_X = test_X.reshape(-1,28*28)
 
 # reshaping the MNIST deataset to fit into our model (hopefully)
-print('X_train: ' + str(train_X.shape))
-print('X_test:  '  + str(test_X.shape))
+with open("full_mnist_evaluations.txt", 'a') as f:
+    print('X_train: ' + str(train_X.shape), file=f)
+    print('X_test:  '  + str(test_X.shape), file=f)
+
+print('X_train: ' + str(train_X.shape),)
+print('X_test:  '  + str(test_X.shape),)
 
 # define constants
 N_FEATURES = 784
 N_OUTPUT = 10
-LEARNING_RATE = 0.02
-EPOCHS = 1000
-TEST_SIZE = 0.4
-BATCH_SIZE = 100
-DEPTH = 3
-WIDTH = 128
+LEARNING_RATE = 0.04
+EPOCHS = 40
+BATCH_SIZE = 300
+DEPTH = 1
+WIDTH = 32
 
 # evaluation function for "hot-one" outputs
 def evaluation_function(x,y):
@@ -56,9 +59,14 @@ mlp = MLP(N_FEATURES, N_OUTPUT, DEPTH, WIDTH,
 
 print('learing...')
 losses, learn_time = mlp.learn(train_X,hot_one_y, EPOCHS, BATCH_SIZE, LEARNING_RATE,print_properties=True)
-print(f"Batch size: {BATCH_SIZE}, Epochs: {EPOCHS}.\nLoss: {losses[-1]}, Learning time: {learn_time:.4f}.")
+with open("full_mnist_evaluations.txt", 'a') as f:
+    print(f"Depth: {DEPTH}, Width: {WIDTH} Learning rate: {LEARNING_RATE}\nBatch size: {BATCH_SIZE}, Epochs: {EPOCHS}.\nLoss: {losses[-1]}, Learning time: {learn_time:.4f}.", file=f)
 
+print(f"Batch size: {BATCH_SIZE}, Epochs: {EPOCHS}.\nLoss: {losses[-1]}, Learning time: {learn_time:.4f}.",)
 
 print('evaluating...')
 accuracy, entropy = mlp.evaluate(train_X, train_y, evaluation_function)
-print(f"Test size: {test_X.shape[0]}\nAccuracy: {accuracy*100:.4f}%, Average entropy: {entropy}")
+with open("full_mnist_evaluations.txt", 'a') as f:
+    print(f"Test size: {test_X.shape[0]}\nAccuracy: {accuracy*100:.4f}%, Average entropy: {entropy}", file=f)
+
+print(f"Test size: {test_X.shape[0]}\nAccuracy: {accuracy*100:.4f}%, Average entropy: {entropy}",)
