f9741e4ea161e2652b097d8e4fd45183be599604
OperationRecon <mohammadnaas203@gmail.com>
2025-02-05 01:44:04 +0200

    edits to policy

--- assignment2/utils/policy.py
+++ assignment2/utils/policy.py
@@ -1,7 +1,7 @@
 import numpy as np
 class Epsilon_Greedy_Policy():
     '''Defines the Epsilon Greedy Policy'''
-    def __init__(self, epsilon=1, min_eps = 0.1, decay=0.992):
+    def __init__(self, epsilon=1, min_eps = 0.1, decay=0.9):
         self.epsilon = epsilon
         self.current_epsilon = epsilon
         self.decay = decay
