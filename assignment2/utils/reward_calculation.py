7f8bf8b4167fc7dd59b506687976c4cfaf8dc555
OperationRecon <mohammadnaas203@gmail.com>
2025-02-06 00:32:34 +0200

    tweaking reward policy

--- assignment2/utils/reward_calculation.py
+++ assignment2/utils/reward_calculation.py
@@ -2,12 +2,15 @@ import numpy as np
 
 def calculate_reward(env, r):
     '''Checks the enviroment for certain conditions in order to modify the reward'''
-    speed = [i.hull.linearVelocity.length for i in env.cars]
+    speed = np.array([i.hull.linearVelocity.length for i in env.cars])
+
+    on_grass = np.array(env.driving_on_grass, np.int8)
 
     # reward speed and progression
-    r +=  (-14 + 1 * np.array(speed)) * 0.2 + (np.array(env.tile_visited_count) * np.array(speed) > 6 * 0.1)
+    r = (r + ((-18 + speed) * 0.05 + np.array(env.tile_visited_count) * np.where(speed > 2, 1,0) * 0.04)) * (1 - on_grass)
 
     # heavily penalise cars for going off track or driving too slowly
-    r = [r[i] -18 if  env.driving_on_grass[i] else r[i] for i in range(len(r))]
+    r += -20 * on_grass
+
 
     return  r
